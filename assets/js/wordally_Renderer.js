186e4842dd316071b46eb9b6055e78e1c2b5e633
Paweł Idzikowski <iddziku@gmail.com>
2020-08-11 20:58:35 +0200

    Implement alert rendering and closing functions to prevent stack

--- assets/js/wordally_Renderer.js
+++ assets/js/wordally_Renderer.js
@@ -34,3 +34,44 @@ function setBasicParameters(element, classes)
     element.setAttribute('onselectstart', 'return false'); 
     return element;
 }
+
+function tryToCloseAlert() {
+    var renderedAlert = document.getElementById('wordally-alert');
+    if(renderedAlert != null) {
+        document.body.removeChild(renderedAlert);
+    }
+}
+
+function renderAlert(message) {
+    tryToCloseAlert();
+
+    var div = document.createElement('div');
+    div.setAttribute('id', 'wordally-alert');
+    div.setAttribute('class', 'alert alert-danger alert-with-icon alert-fixed')
+
+    var dismissButton = document.createElement('button');
+    dismissButton.setAttribute('class', 'close');
+    dismissButton.setAttribute('data-dismiss', 'alert');
+    dismissButton.setAttribute('aria-hidden', 'true');
+    dismissButton.setAttribute('aria-label', 'close');
+
+    var dismissButtonIcon = document.createElement('i');
+    dismissButtonIcon.setAttribute('class', 'tim-icons icon-simple-remove');
+
+    dismissButton.appendChild(dismissButtonIcon);
+
+    div.appendChild(dismissButton);
+
+    var spanIcon = document.createElement('span');
+    spanIcon.setAttribute('data-notify', 'icon');
+    spanIcon.setAttribute('class', 'tim-icons icon-alert-circle-exc');
+
+    div.appendChild(spanIcon);
+
+    var spanMessage = document.createElement('span');
+    spanMessage.innerHTML = `<strong>Oops</strong> <br/> ${message}`;
+
+    div.appendChild(spanMessage);
+
+    document.body.appendChild(div);
+}
